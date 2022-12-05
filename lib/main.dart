7574d28f54aebdb6f6cde9585b73b34171eb07db
buttonsrtoys <buttonsrtoys@gmail.com>
2022-12-05 10:49:38 -0400

    refactored for notifyListeners

--- lib/main.dart
+++ lib/main.dart
@@ -11,33 +11,24 @@ Widget myApp() => Bilocator<ColorService>(
     child: MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Bilocator<ColorService>(
-          builder: () => ColorService(milliSeconds: 2250),
-          location: Location.tree,
-          child: CounterPage()),
+          builder: () => ColorService(milliSeconds: 2250), location: Location.tree, child: CounterPage()),
     ));
 
 class CounterPage extends View<CounterPageViewModel> {
-  CounterPage({super.key})
-      : super(
-            builder: () => CounterPageViewModel(), location: Location.registry);
+  CounterPage({super.key}) : super(builder: () => CounterPageViewModel(), location: Location.registry);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Center(
-            child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-              Text(viewModel.letterCount.value,
-                  style: TextStyle(
-                    fontSize: 64,
-                    color: listenTo<ColorService>(context: context).color.value,
-                  )),
-              Text(viewModel.numberCount.toString(),
-                  style: TextStyle(
-                      fontSize: 64,
-                      color: listenTo<ColorService>().color.value)),
-            ])),
+            child: Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+          Text(viewModel.letterCount.value,
+              style: TextStyle(
+                fontSize: 64,
+                color: listenTo<ColorService>(context: context).color,
+              )),
+          Text(viewModel.numberCount.toString(), style: TextStyle(fontSize: 64, color: listenTo<ColorService>().color)),
+        ])),
         floatingActionButton: IncrementButton());
   }
 }
@@ -51,14 +42,12 @@ class CounterPageViewModel extends ViewModel {
     buildView();
   }
 
-  void incrementLetter() => letterCount.value = letterCount.value == 'z'
-      ? 'a'
-      : String.fromCharCode(letterCount.value.codeUnits[0] + 1);
+  void incrementLetter() =>
+      letterCount.value = letterCount.value == 'z' ? 'a' : String.fromCharCode(letterCount.value.codeUnits[0] + 1);
 }
 
 class IncrementButton extends View<IncrementButtonViewModel> {
-  IncrementButton({super.key})
-      : super(builder: () => IncrementButtonViewModel());
+  IncrementButton({super.key}) : super(builder: () => IncrementButtonViewModel());
 
   @override
   Widget build(BuildContext context) {
@@ -73,9 +62,7 @@ class IncrementButtonViewModel extends ViewModel {
   late final isNumber = createProperty<bool>(false);
   String get buttonText => isNumber.value ? '+1' : '+a';
   void incrementCounter() {
-    isNumber.value
-        ? get<CounterPageViewModel>().incrementNumber()
-        : get<CounterPageViewModel>().incrementLetter();
+    isNumber.value ? get<CounterPageViewModel>().incrementNumber() : get<CounterPageViewModel>().incrementLetter();
     isNumber.value = !isNumber.value;
   }
 }
@@ -83,17 +70,13 @@ class IncrementButtonViewModel extends ViewModel {
 class ColorService extends Model {
   ColorService({required int milliSeconds}) {
     _timer = Timer.periodic(Duration(milliseconds: milliSeconds), (_) {
-      color.value = <Color>[
-        Colors.red,
-        Colors.black,
-        Colors.blue,
-        Colors.orange
-      ][++_counter % 4];
+      color = <Color>[Colors.red, Colors.black, Colors.blue, Colors.orange][++_counter % 4];
+      notifyListeners();
     });
   }
 
   int _counter = 0;
-  late final color = createProperty<Color>(Colors.orange);
+  Color color = Colors.orange;
   late Timer _timer;
 
   @override
