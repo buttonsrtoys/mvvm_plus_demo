ac3b799a8e81500cad2e04fd5a2be3198b1fb93d
buttonsrtoys <buttonsrtoys@gmail.com>
2022-12-17 10:59:15 -0400

    added 3rd lower/upper case letters

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,9 @@ import 'package:bilocator/bilocator.dart';
 void main() => runApp(myApp());
 
 Widget myApp() => Bilocator<ColorService>(
-    builder: () => ColorService(milliSeconds: 1500),
-    child: MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Bilocator<ColorService>(
-          builder: () => ColorService(milliSeconds: 2250), location: Location.tree, child: CounterPage()),
-    ));
+      builder: () => ColorService(),
+      child: MaterialApp(debugShowCheckedModeBanner: false, home: CounterPage()),
+    );
 
 class CounterPage extends View<CounterPageViewModel> {
   CounterPage({super.key}) : super(builder: () => CounterPageViewModel(), location: Location.registry);
@@ -22,12 +19,7 @@ class CounterPage extends View<CounterPageViewModel> {
     return Scaffold(
         body: Center(
             child: Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-          Text(viewModel.letterCount.value,
-              style: TextStyle(
-                fontSize: 64,
-                color: listenTo<ColorService>(context: context).color,
-              )),
-          Text(viewModel.numberCount.toString(), style: TextStyle(fontSize: 64, color: listenTo<ColorService>().color)),
+          Text(viewModel.text, style: TextStyle(fontSize: 64, color: listenTo<ColorService>().color)),
         ])),
         floatingActionButton: IncrementButton());
   }
@@ -35,15 +27,22 @@ class CounterPage extends View<CounterPageViewModel> {
 
 class CounterPageViewModel extends ViewModel {
   int numberCount = 0;
-  late final letterCount = ValueNotifier<String>('a')..addListener(buildView);
+  late final lowercaseCount = ValueNotifier<String>('a')..addListener(buildView);
+  late final uppercaseCount = Property<String>('A')..addListener(buildView);
+  // late final uppercaseCount = createProperty('A');
 
   void incrementNumber() {
-    numberCount = numberCount == 9 ? 0 : numberCount + 1;
+    numberCount = numberCount == 25 ? 0 : numberCount + 1;
     buildView();
   }
 
-  void incrementLetter() =>
-      letterCount.value = letterCount.value == 'z' ? 'a' : String.fromCharCode(letterCount.value.codeUnits[0] + 1);
+  void incrementUppercase() => uppercaseCount.value =
+      uppercaseCount.value == 'Z' ? 'A' : String.fromCharCode(uppercaseCount.value.codeUnits[0] + 1);
+
+  void incrementLowercase() => lowercaseCount.value =
+      lowercaseCount.value == 'z' ? 'a' : String.fromCharCode(lowercaseCount.value.codeUnits[0] + 1);
+
+  String get text => numberCount.toString() + uppercaseCount.value + lowercaseCount.value;
 }
 
 class IncrementButton extends View<IncrementButtonViewModel> {
@@ -59,17 +58,21 @@ class IncrementButton extends View<IncrementButtonViewModel> {
 }
 
 class IncrementButtonViewModel extends ViewModel {
-  late final isNumber = createProperty<bool>(false);
-  String get buttonText => isNumber.value ? '+1' : '+a';
+  late final index = createProperty<int>(0);
+  String get buttonText => <String>['+1', '+A', '+a'][index.value];
   void incrementCounter() {
-    isNumber.value ? get<CounterPageViewModel>().incrementNumber() : get<CounterPageViewModel>().incrementLetter();
-    isNumber.value = !isNumber.value;
+    <void Function()>[
+      get<CounterPageViewModel>().incrementNumber,
+      get<CounterPageViewModel>().incrementUppercase,
+      get<CounterPageViewModel>().incrementLowercase,
+    ][index.value]();
+    index.value = index.value == 2 ? 0 : index.value + 1;
   }
 }
 
 class ColorService extends Model {
-  ColorService({required int milliSeconds}) {
-    _timer = Timer.periodic(Duration(milliseconds: milliSeconds), (_) {
+  ColorService() {
+    _timer = Timer.periodic(const Duration(milliseconds: 1000), (_) {
       color = <Color>[Colors.red, Colors.black, Colors.blue, Colors.orange][++_counter % 4];
       notifyListeners();
     });
